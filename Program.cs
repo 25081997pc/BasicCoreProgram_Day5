80e822fa95620ab0ae400a06dec38d81f4a3df62
25081997pc <chavanpooja2508@gmail.com>
2022-05-26 15:07:24 +0530

    Adding Quotient and Remainder

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace BasicCoreProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //VowelOrConstant.CheckVowelOrConstant();
+            //SwapTwoNumber.SwappingNumbers();
+            //Division.CalculateQuotientAndRemainder();
+            //EvenorOdd.CheckNumberEvenorOdd();
+            //LargestNumber.FindingLargestNumber();
+
+            //PerfectNumber.FindingPerfectNumber();
+            //ReverseNumber.ReverseTheNumber();
+            PrimeNumber.PrintThePrimeNum();
+            //Fibonacci.PrintingFibonacci();
+
+
+
+
+
+        }
+    }
+
+}
